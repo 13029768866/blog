b0ed35eb300b08c3fb309914a375ccbddf8b656f
woodensRangerWZJ <13029768866@163.com>
2020-01-14 23:21:24 +0800

    左右布局+首页表单

---
+++ pages/list.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Head from 'next/head'
+import Header from '../components/header'
+import {Row,Col} from "antd";
+
+
+const List = () => (
+    <div>
+        <Head>
+            <title>List</title>
+            <link rel="icon" href="/favicon.ico" />
+            <Header/>
+            <Row
+                className="comm-main"
+                type="flex"
+                justify="center"
+            >
+                <Col
+                    className="comm-left"
+                    xs={24}
+                    sm={24} md={16} lg={18} xl={14}>
+                    左侧
+                </Col>
+                <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+                    右侧
+                </Col>
+            </Row>
+        </Head>
+    </div>
+)
+
+export default List
